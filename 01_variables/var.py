ccfc716b607644e832b983fad2ae7d417fd77b28
Varenya Visen <varenyavisen@gmail.com>
2025-08-08 10:23:58 +0530

    variables

---
+++ 01_variables/var.py
@@ -0,0 +1,37 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the calue it containes
+
+
+# strings
+first_name = "Varenya Visen"
+food = "pizza"
+email = "fake@gmail.com"
+print(first_name)
+
+
+#Integers  - whole numbers
+age = 22
+quantity = 3
+
+# Float
+price = 10.99
+gpa = 3.2
+
+# boolean
+is_student = False
+is_graduate = True
+
+# If condition
+if is_student:
+    print("You are a student")
+else:
+    print("you are not a student")
+
+# f-string to print customize output
+print(f"Hello {first_name}")
+print(f"you are {age} years old")
+print(f"you are buying {quantity} items")
+print(f"The price will be {price}$")
+print(f"You like {food}")
+print(f" Your email is {email}")
+print(f"are you a student?: {is_student}")
