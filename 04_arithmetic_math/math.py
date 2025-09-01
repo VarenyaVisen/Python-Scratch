490ed7d8f520de3e93b43b51248b8fb915af2426
Varenya Visen <varenyavisen@gmail.com>
2025-09-01 12:01:10 +0530

    some in built math functions and math module

---
+++ 04_arithmetic_math/math.py
@@ -0,0 +1,23 @@
+x = 3.14
+y = -4
+z = 5
+
+# Some built in methods
+result = round(x)
+result2 = abs(y)
+result3 = pow(4,3)
+result4 = max(x,y,z)
+result5 = min(x,y,z)
+
+print(result)
+
+# Math module
+import math
+
+print(math.pi)
+print(math.e)
+result6 = math.sqrt(9)
+print(result6)
+
+# math.ceil  = it will round up to the higher number
+# math.floor = it will round up to the lower number
