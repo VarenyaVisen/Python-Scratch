395f222c7ed436c5f6e7adcc801de87022a4a212
Varenya Visen <varenyavisen@gmail.com>
2025-09-01 13:40:27 +0530

    conditional statements

---
+++ 05_conditional_statement/main.py
@@ -0,0 +1,22 @@
+# if = Do some code only IF some condition is True
+#      Else do something else
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(f"You are eligible for the credit card")
+
+elif age < 0 :
+    print("Invalid age!!!")
+
+else:
+    print("Sorry you're not eligible")
+
+
+
+response = input("Enter your response : ").lower
+if response == "y":
+    print("You will get the food")
+
+else:
+    print("You will not get the food")
